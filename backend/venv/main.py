b5c6514e37d2f7681b6dbe61633f3467d86cd6fb
SeanChen2 <seanchen.sc2@gmail.com>
2024-08-17 00:43:28 -0400

    Initial installations and format

---
+++ backend/venv/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Create these "routes" for the frontend to "GET" information from the backend or "POST" (give) information to the backend.
+# This route returns JSON which can be used by the frontend (to be displayed)
+@app.route("/api/suggested_tutors", methods=['GET'])
+def suggested_tutors():
+    return jsonify(
+        {
+            "tutors": [
+                # List of suggested tutors to return here
+            ]
+        }
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Text goes here</h1>
+      </div>
+    </>
+  )
+}
