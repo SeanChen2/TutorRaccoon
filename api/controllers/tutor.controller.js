01aedc4fac09705764213e84633acaac451f0b2d
Anania17 <ananiayohannesz17@gmail.com>
2024-08-18 01:09:59 -0400

    More routes created

--- api/controllers/tutor.controller.js
+++ api/controllers/tutor.controller.js
@@ -5,7 +5,7 @@ exports.addTutor =  async (req,res) => {
         name: req.body.name,
         institution: req.body.institution,
         courses: req.body.courses,
-        style: req.body.teachingStyle,
+        style: req.body.style,
         sessions: req.body.sessions,
         availability: req.body.availability,
         zip: req.body.zip,
@@ -70,3 +70,139 @@ exports.getTutorBySession = async(req,res) => {
         });
         }
 }
+
+exports.getAllStyles = async (req,res) => {
+    try{
+        const style = await Tutor.distinct('style');
+        res.status(200).send(style);
+
+    }catch(err){
+        console.log('Tutor style could  not be fetched', err.message);
+        res.status(500).send('Tutor style could  not be fetched');
+
+    }
+}
+
+exports.getTutorByStyle = async(req,res) => {
+    const { styleName } = req.params;
+    try {
+        const tutors =  await Tutor.find({
+            style: styleName
+        });
+        res.status(200).send(tutors);
+
+    }catch(err){
+        console.log('Tutor could not be fetched using style',err.message);
+        res.status(500).send({
+            message: 'Tutor could not be fetched using style'
+        
+        });
+        }
+}
+
+exports.getAllCourses = async (req,res) => {
+    try{
+        const courses = await Tutor.distinct('courses');
+        res.status(200).send(courses);
+
+    }catch(err){
+        console.log('Tutor courses could  not be fetched', err.message);
+        res.status(500).send('Tutor courses could  not be fetched');
+
+    }
+}
+
+
+exports.getTutorByCourse = async(req,res) => {
+    const { courseName } = req.params;
+    try {
+        const tutors =  await Tutor.find({
+            courses: courseName
+        });
+        res.status(200).send(tutors);
+
+    }catch(err){
+        console.log('Tutor could not be fetched using courses',err.message);
+        res.status(500).send({
+            message: 'Tutor could not be fetched using courses'
+        
+        });
+        }
+}
+
+exports.getAllInstitution = async (req,res) => {
+    try{
+        const institution = await Tutor.distinct('institution');
+        res.status(200).send(institution);
+
+    }catch(err){
+        console.log('Tutor institution could  not be fetched', err.message);
+        res.status(500).send('Tutor institution could  not be fetched');
+
+    }
+}
+
+
+exports.getTutorByInstitution = async(req,res) => {
+    const { institutionName } = req.params;
+    try {
+        const tutors =  await Tutor.find({
+            institution: institutionName
+        });
+        res.status(200).send(tutors);
+
+    }catch(err){
+        console.log('Tutor could not be fetched using institution',err.message);
+        res.status(500).send({
+            message: 'Tutor could not be fetched using institution'
+        
+        });
+        }
+}
+
+
+
+exports.getAllAvailability = async (req,res) => {
+    try{
+        const availability = await Tutor.distinct('availability');
+        res.status(200).send(availability);
+
+    }catch(err){
+        console.log('Tutor availability could  not be fetched', err.message);
+        res.status(500).send('Tutor availability could  not be fetched');
+
+    }
+}
+
+
+exports.getTutorByAvailability = async(req,res) => {
+    const { availabilityName } = req.params;
+    try {
+        const tutors =  await Tutor.find({
+            availability: availabilityName
+        });
+        res.status(200).send(tutors);
+
+    }catch(err){
+        console.log('Tutor could not be fetched using availability',err.message);
+        res.status(500).send({
+            message: 'Tutor could not be fetched using availability'
+        
+        });
+        }
+}
+
+
+
+exports.getAllZip = async (req,res) => {
+    try{
+        const zip = await Tutor.distinct('zip');
+        res.status(200).send(zip);
+
+    }catch(err){
+        console.log('Tutor zip could  not be fetched', err.message);
+        res.status(500).send('Tutor zip could  not be fetched');
+
+    }
+}
+
--- api/routes/tutorRoute.js
+++ api/routes/tutorRoute.js
@@ -5,6 +5,13 @@ module.exports = function(app) {
     app.get('/api/tutor',tutorController.getAllTutors);
     app.get('/api/tutor/sessions',tutorController.getAllSessions);
     app.get('/api/tutor/sessions/:sessionName',tutorController.getTutorBySession);
+    app.get('/api/tutor/style',tutorController.getAllStyles);
+    app.get('/api/tutor/style/:styleName',tutorController.getTutorByStyle);
+    app.get('/api/tutor/courses',tutorController.getAllCourses);
+    app.get('/api/tutor/courses/:courseName',tutorController.getTutorByCourse);
+    app.get('/api/tutor/institution',tutorController.getAllInstitution);
+    app.get('/api/tutor/institution/:institutionName',tutorController.getTutorByInstitution);
+    app.get('/api/tutor/zip',tutorController.getAllZip);
 
 
 }
